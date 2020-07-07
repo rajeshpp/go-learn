5d7eacb1e963a7a2b5470bd563e1a702805d1b31
Rajesh <19406666+rajeshpp@users.noreply.github.com>
2020-07-07 20:01:26 +0530

    Create time_v2.go

---
+++ time_v2.go
@@ -0,0 +1,17 @@
+package main
+
+import ("fmt";"time")
+
+func main() {
+	fmt.Println("Welcome to the playground!")
+
+	fmt.Println("The time is", time.Now())
+}
+
+
+
+"""
+Output:
+Welcome to the playground!
+The time is 2020-07-07 20:01:00.1903924 +0530 IST
+"""
